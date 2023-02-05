58861e87da588d190f6b04e1e8c5ccca5e594e28
Sowren Sen <80112748+nemoitis@users.noreply.github.com>
2023-02-05 22:02:37 +0600

    renamed facade

--- src/Facades/Svg.php
+++ src/Facades/Svg.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @see \Sowren\SvgAvatarGenerator\SvgAvatarGenerator
  */
-class SvgAvatarGenerator extends Facade
+class Svg extends Facade
 {
     protected static function getFacadeAccessor()
     {
