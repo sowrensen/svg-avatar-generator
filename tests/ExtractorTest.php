877b056b21e2bd7b0d31e62cc9bf29150c7e18bd
Sowren Sen <80112748+nemoitis@users.noreply.github.com>
2023-02-09 16:23:37 +0600

    wip

--- tests/ExtractorTest.php
+++ tests/ExtractorTest.php
@@ -1,16 +1,16 @@
 <?php
 
-test('it can extract initials from multiple words', function () {
+it('can extract initials from multiple words', function () {
     $generator = new \Sowren\SvgAvatarGenerator\SvgAvatarGenerator('George R. R. Martin');
     expect($generator->getInitials())->toBe('GM');
 });
 
-test('it can extract initials from single word', function () {
+it('can extract initials from single word', function () {
     $generator = new Sowren\SvgAvatarGenerator\SvgAvatarGenerator('Eddard');
     expect($generator->getInitials())->toBe('ED');
 });
 
-test('it can extract initials from studly cased word', function () {
+it('can extract initials from studly cased word', function () {
     $generator = new Sowren\SvgAvatarGenerator\SvgAvatarGenerator('EddardStark');
     expect($generator->getInitials())->toBe('ES');
 });
---
+++ tests/UrlTest.php
@@ -0,0 +1,7 @@
+<?php
+
+it('will generate a url with the passed name', function() {
+    $svg = new Sowren\SvgAvatarGenerator\SvgAvatarGenerator('Jon Snow');
+
+    expect($svg->toUrl())->toBe('http://localhost/svg-avatar?text=Jon Snow');
+});
