1e02848125caa4ef205c6af05f0564bddf9cda96
sowrensen <sowrensen@users.noreply.github.com>
2023-02-09 10:24:26 +0000

    Fix styling

--- tests/UrlTest.php
+++ tests/UrlTest.php
@@ -1,6 +1,6 @@
 <?php
 
-it('will generate a url with the passed name', function() {
+it('will generate a url with the passed name', function () {
     $svg = new Sowren\SvgAvatarGenerator\SvgAvatarGenerator('Jon Snow');
 
     expect($svg->toUrl())->toBe('http://localhost/svg-avatar?text=Jon Snow');
