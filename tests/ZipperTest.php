0e5d0c6f2d7afcc779a0c3880bb794c6ea111b58
sowrensen <sowrensen@users.noreply.github.com>
2023-02-25 18:13:49 +0000

    Fix styling

--- tests/ZipperTest.php
+++ tests/ZipperTest.php
@@ -2,7 +2,7 @@
 
 use Sowren\SvgAvatarGenerator\SvgAvatarGenerator;
 
-it('can make gradient set from single set of colors', function() {
+it('can make gradient set from single set of colors', function () {
     $generator = new SvgAvatarGenerator();
     $generator->setGradientColors('red', 'green', 'blue')
         ->setGradientStops(0, .5, 1);
@@ -11,12 +11,12 @@ it('can make gradient set from single set of colors', function() {
         [
             ['color' => 'red', 'offset' => 0],
             ['color' => 'green', 'offset' => .5],
-            ['color' => 'blue', 'offset' => 1]
-        ]
+            ['color' => 'blue', 'offset' => 1],
+        ],
     ]);
 });
 
-it('can make gradient set from multiple sets of colors', function() {
+it('can make gradient set from multiple sets of colors', function () {
     $generator = new SvgAvatarGenerator();
     $generator->setGradientColors(['red', 'green', 'yellow'], 'blue')
         ->setGradientStops(0, .5, 1);
@@ -25,18 +25,18 @@ it('can make gradient set from multiple sets of colors', function() {
         [
             ['color' => 'red', 'offset' => 0],
             ['color' => 'green', 'offset' => .5],
-            ['color' => 'yellow', 'offset' => 1]
+            ['color' => 'yellow', 'offset' => 1],
         ],
         [
             ['color' => 'blue', 'offset' => 0],
             ['color' => 'blue', 'offset' => .5],
-            ['color' => 'blue', 'offset' => 1]
-        ]
+            ['color' => 'blue', 'offset' => 1],
+        ],
     ]);
 });
 
-it('will omit additional colors set more than offsets', function() {
-     $generator = new SvgAvatarGenerator();
+it('will omit additional colors set more than offsets', function () {
+    $generator = new SvgAvatarGenerator();
     $generator->setGradientColors(['red', 'green', 'yellow'])
         ->setGradientStops(0, 1);
 
@@ -44,12 +44,12 @@ it('will omit additional colors set more than offsets', function() {
         [
             ['color' => 'red', 'offset' => 0],
             ['color' => 'green', 'offset' => 1], // yellow after green is omitted
-        ]
+        ],
     ]);
 });
 
-it('will repeat last color if set fewer than offsets', function() {
-     $generator = new SvgAvatarGenerator();
+it('will repeat last color if set fewer than offsets', function () {
+    $generator = new SvgAvatarGenerator();
     $generator->setGradientColors(['red', 'green'], 'blue')
         ->setGradientStops(0, .5, 1);
 
@@ -63,6 +63,6 @@ it('will repeat last color if set fewer than offsets', function() {
             ['color' => 'blue', 'offset' => 0],
             ['color' => 'blue', 'offset' => .5], // repeating blue
             ['color' => 'blue', 'offset' => 1], // repeating blue
-        ]
+        ],
     ]);
 });
