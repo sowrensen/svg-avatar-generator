481362c89e7101a63fbbc46c97dc4dcc2fabb9a4
sowrensen <sowrensen@users.noreply.github.com>
2023-02-26 07:36:26 +0000

    Fix styling

--- src/Exceptions/InvalidGradientStopException.php
+++ src/Exceptions/InvalidGradientStopException.php
@@ -6,7 +6,8 @@ class InvalidGradientStopException extends \Exception
 {
     public static function create(array $offsets): InvalidGradientStopException
     {
-        $values = implode(',',  $offsets);
+        $values = implode(',', $offsets);
+
         return new self("Invalid value [{$values}] for gradient stop is provided. Gradient stops must be between 0 and 1.");
     }
 }
