6a06629887a49ae7d54ff8aac3a9e7d070b2adae
Sowren Sen <80112748+nemoitis@users.noreply.github.com>
2024-03-05 12:52:49 +0600

    use regex to match validation messages in tests

--- tests/ExceptionTest.php
+++ tests/ExceptionTest.php
@@ -6,49 +6,49 @@ use Sowren\SvgAvatarGenerator\SvgAvatarGenerator;
 it('will throw exception if svg size less than minimum is provided', function () {
     config(['svg-avatar.size' => 8]);
     new SvgAvatarGenerator('Rob Stark');
-})->throws(ValidationException::class, 'The svg size field must be between 16 and 512.');
+})->throws(ValidationException::class)->expectExceptionMessageMatches('/The svg size(?: field)? must be between 16 and 512/');
 
 it('will throw exception if svg size greater than maximum is provided', function () {
     config(['svg-avatar.size' => 1024]);
     new SvgAvatarGenerator('Jon Snow');
-})->throws(ValidationException::class, 'The svg size field must be between 16 and 512.');
+})->throws(ValidationException::class)->expectExceptionMessageMatches('/The svg size(?: field)? must be between 16 and 512/');
 
 it('will throw exception if font size less than minimum is provided', function () {
     config(['svg-avatar.font_size' => 9]);
     new SvgAvatarGenerator('Sansa Stark');
-})->throws(ValidationException::class, 'The font size field must be between 10 and 50.');
+})->throws(ValidationException::class)->expectExceptionMessageMatches( '/The font size(?: field)? must be between 10 and 50/');
 
 it('will throw exception if font size greater than maximum is provided', function () {
     config(['svg-avatar.font_size' => 51]);
     new SvgAvatarGenerator('Arya Stark');
-})->throws(ValidationException::class, 'The font size field must be between 10 and 50.');
+})->throws(ValidationException::class)->expectExceptionMessageMatches( '/The font size(?: field)? must be between 10 and 50/');
 
 it('will throw exception if gradient rotation less than minimum is provided', function () {
     config(['svg-avatar.gradient_rotation' => -1]);
     new SvgAvatarGenerator('Brandon Stark');
-})->throws(ValidationException::class, 'The gradiant rotation field must be between 0 and 360.');
+})->throws(ValidationException::class)->expectExceptionMessageMatches( '/The gradiant rotation(?: field)? must be between 0 and 360/');
 
 it('will throw exception if gradient rotation greater than maximum is provided', function () {
     config(['svg-avatar.gradient_rotation' => 361]);
     new SvgAvatarGenerator('Rickon Stark');
-})->throws(ValidationException::class, 'The gradiant rotation field must be between 0 and 360.');
+})->throws(ValidationException::class)->expectExceptionMessageMatches( '/The gradiant rotation(?: field)? must be between 0 and 360/');
 
 it('will throw missing text exception if text is not set', function () {
     $generator = new SvgAvatarGenerator();
     $generator->getInitials();
-})->throws(ValidationException::class, 'The svg text field is required.');
+})->throws(ValidationException::class, 'The svg text field is required');
 
 it('will throw exception if gradient stop less than minimum is provided', function () {
     config(['svg-avatar.gradient_stops' => [-1, 0.5]]);
     new SvgAvatarGenerator('Lyanna Stark');
-})->throws(ValidationException::class, 'The gradiant_stops.0 field must be between 0 and 1.');
+})->throws(ValidationException::class)->expectExceptionMessageMatches('/The gradiant_stops.0(?: field)? must be between 0 and 1/');
 
 it('will throw exception if gradient stop greater than maximum is provided', function () {
     config(['svg-avatar.gradient_stops' => [0.2, 2]]);
     new SvgAvatarGenerator('Benjen Stark');
-})->throws(ValidationException::class, 'The gradiant_stops.1 field must be between 0 and 1.');
+})->throws(ValidationException::class)->expectExceptionMessageMatches('/The gradiant_stops.1(?: field)? must be between 0 and 1/');
 
 it('will throw exception if invalid font url is provided', function () {
     config(['svg-avatar.custom_font_url' => 'invalid_url']);
     new SvgAvatarGenerator('Rickard Stark');
-})->throws(ValidationException::class, 'The custom font url field must be a valid URL.');
+})->throws(ValidationException::class)->expectExceptionMessageMatches('/The custom font url(?: field)? must be a valid URL/');
